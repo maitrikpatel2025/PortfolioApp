804c18a98ae6f6020c62c2474a006fdb4d8627e2
Dhruvika Kharva <59853047+maitrikpatel2025@users.noreply.github.com>
2021-04-15 16:43:04 -0400

    Update ProjectsList.js

--- client/src/Components/Manager/Projects/ProjectsList.js
+++ client/src/Components/Manager/Projects/ProjectsList.js
@@ -1,86 +1,83 @@
 
-  
-import React, { Component }   from "react";
-import { connect }            from "react-redux";
-import { Link }               from "react-router-dom";
 
-import { fetchProjects }       from "../../../Action/projects";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+import { fetchProjects } from "../../../Action/projects";
 
 
 class ProjectsList extends Component {
 
-  componentDidMount() {
-    this.props.fetchProjects();
-  }
+    componentDidMount() {
+        this.props.fetchProjects();
+    }
 
-  renderAdmin(project) {
-      return (
-        <div style={{ textAlign: "left" }}>
-          <Link to={`projects/edit/${project._id}`} className="ui button primary">
-            EDIT
+    renderAdmin(project) {
+        return (
+            <div className="ui two buttons">
+                <Link to={`projects/edit/${project._id}`} className="ui button primary">
+                    EDIT
           </Link>
-          <Link
-            to={`projects/delete/${project._id}`}
-            className="ui button negative"
-          >
-            DELETE
+                <Link
+                    to={`projects/delete/${project._id}`}
+                    className="ui button negative"
+                >
+                    DELETE
           </Link>
-        </div>
-      );
-  }
+            </div>
+        );
+    }
 
-  renderList() {
-    return this.props.projects.map((project) => {
-      return (
-        <div className="item">
-          <div className="list-group list-group-flush " key={project._id}>
-            <li className="list-group-item list-group-item-action">
-              <h2 className="mb-1">
-                <i className="icon camera" />
-                <Link to={`/projects/${project._id}`}>
-                {project.title}
+    renderList() {
+        return this.props.projects.map((project) => {
+            return (
+                <div className="card">
+                    <div className="content" key={project._id}>
+
+                        <div className="header">
+                            <Link to={`/projects/${project._id}`}>
+                                {project.title}
+                            </Link>
+                        </div>
+                        <p className="description">{project.summary}</p>
+                        {this.renderAdmin(project)}
+
+                    </div>
+                </div>
+            );
+        });
+    }
+
+    renderCreate() {
+
+        return (
+            <div style={{ textAlign: "center", padding: "20px" }}>
+                <Link to={`projects/new`}>
+                    <button className="ui button purple">Create live project</button>
                 </Link>
-              </h2>
-              <p>{project.description}</p>
-              {this.renderAdmin(project)}
-            </li>
-          </div>
-        </div>
-      );
-    });
-  }
+            </div>
+        );
 
-  renderCreate() {
-   
-      return (
-        <div style={{ textAlign: "right" }}>
-          <Link to={`projects/new`}>
-            <button className="ui button primary">Create live project</button>
-          </Link>
-        </div>
-      );
-    
-  }
+    }
 
-  render() {
-    return (
-      <div className="ProjectsList">
-        <div className="ui container">
-          <h1>List of all project </h1>
-          <div className="ui celled list">{this.renderList()}</div>
-          {this.renderCreate()}
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="ProjectsList">
+                <h1 style={{ textAlign: "center" }} >List of all project </h1>
+                {this.renderCreate()}
+                <div className="ui cards">{this.renderList()}</div>
+            </div>
+        );
+    }
 
 }
 
 
 const mapStateToProps = (state) => {
-  return {
-    projects: Object.values(state.project),
-  };
+    return {
+        projects: Object.values(state.project),
+    };
 };
 
 
