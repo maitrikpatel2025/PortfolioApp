fb2dfb0b67287afe3411f760fb0d38763ead473a
Dhruvika Kharva <59853047+maitrikpatel2025@users.noreply.github.com>
2021-05-06 23:20:19 -0400

    make it beftter

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,7 +7,7 @@ const requireSignin = passport.authenticate("local", { session: false });
 
 module.exports = function (app) {
   // auth routes
-  app.get("/", requireAuth, function (req, res) {
+  app.get("/", function (req, res) {
     res.send({ hi: "there" });
   });
   app.post("/signin", requireSignin, Authentication.signin);
