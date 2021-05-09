9297f6370acc10daff407d6759c5bf5294bda1a6
Dhruvika Kharva <59853047+maitrikpatel2025@users.noreply.github.com>
2021-05-09 09:41:28 -0400

    make it befttesfgr

---
+++ server/client/build/precache-manifest.34bf60172a95773d4725bd356882900d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d6fb5b157b692edc2f8a73bb9037f58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "859421d8c7213a73f08e",
+    "url": "/static/css/main.a3315802.chunk.css"
+  },
+  {
+    "revision": "e63eda6a1e2af7c0a9d9",
+    "url": "/static/js/2.c552ef62.chunk.js"
+  },
+  {
+    "revision": "e3cddcc14d0b349b4e6eccba623e55ae",
+    "url": "/static/js/2.c552ef62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "859421d8c7213a73f08e",
+    "url": "/static/js/main.20aefc27.chunk.js"
+  },
+  {
+    "revision": "1b458cbf1e0582e68774",
+    "url": "/static/js/runtime-main.b2a5e300.js"
+  },
+  {
+    "revision": "351322cd7a1368a6bb3f4415e8863abe",
+    "url": "/static/media/LandingHello.351322cd.png"
+  },
+  {
+    "revision": "bc43d7a1c1b6e207a6465f1a041f2ab1",
+    "url": "/static/media/LandingMobile.bc43d7a1.png"
+  },
+  {
+    "revision": "b8b8ca6d704ef3baa9b31c955fbafb8f",
+    "url": "/static/media/Logo.b8b8ca6d.svg"
+  },
+  {
+    "revision": "42f2d2be179fb107aa38a6243ffa9c91",
+    "url": "/static/media/Photo.42f2d2be.jpeg"
+  }
+]);
