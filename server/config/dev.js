be89a2e0bec3bf0afabff1d9c8d7a454d5e3664b
Dhruvika Kharva <59853047+maitrikpatel2025@users.noreply.github.com>
2021-05-09 09:48:14 -0400

    make it befttesfgrsdfj

---
+++ server/config/dev.js
@@ -0,0 +1,5 @@
+  module.exports = {
+    mongoURI : 'mongodb+srv://maitrik:30112025@portfoiloapp.pxv03.mongodb.net/maitrik?retryWrites=true&w=majority',
+    cookiekey: 'q874yqeuiqhkehq834',
+    secret: 'sdjhjhakfkhkjghjgkgfghfjgfkljhjshgdjfakd',
+} 
